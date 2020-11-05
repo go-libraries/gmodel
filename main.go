8e52b9a4f4b6e28442775b9c1a621e6a868ccf36
李文龙 <liwenlong@himango.com>
2020-11-05 11:50:09 +0800

    gmodel

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/go-libraries/genModels"
 	"os"
+	"strings"
 )
 
 var (
@@ -14,6 +15,7 @@ var (
 	ignoreTable string
 	style       string
 	packageName string
+	camel       string
 	help        bool
 	h           bool
 )
@@ -23,8 +25,9 @@ func init() {
 	flag.StringVar(&modelPath, "dir", currentPath, "a dir name save model file path, default is current path")
 	flag.StringVar(&driver, "driver", "mysql", "database driver,like `mysql` `mariadb`, default is mysql")
 	flag.StringVar(&dsn, "dsn", "", "connection info names dsn")
-	flag.StringVar(&ignoreTable, "ig_tables", "", "ignore table names")
+	flag.StringVar(&ignoreTable, "ig_tables", "", "ignore table names, like [tableA,tableB]")
 	flag.StringVar(&style, "style", "default", "use orm style like `bee` `gorm`, default `default`")
+	flag.StringVar(&camel, "camel", "0", "json use camel 0 false 1 true")
 	flag.StringVar(&packageName, "package", "", "help")
 	flag.BoolVar(&help, "help", false, "this help")
 	flag.BoolVar(&h, "h", false, "this help")
@@ -32,15 +35,36 @@ func init() {
 
 func main() {
 	flag.Parse()
-	if h {
+	if h || help {
 		flag.Usage()
 	}
-
+	//dsn = "ceshiceshi:mgtj123456@tcp(rm-ly29w98y58jjy4s7x.mysql.rds.aliyuncs.com:3306)/mgtj_app_content?charset=utf8mb4"
+	//driver = "mysql"
+	//style = "grom"
+	//packageName = "content"
+	//modelPath = "d:\\work\\contents"
 	if dsn == "" {
 		flag.Usage()
 	}
+
+	if modelPath != "" {
+		_,e := os.Stat(modelPath)
+		if e != nil {
+			_ = os.Mkdir(modelPath, os.ModePerm)
+		}
+	}
 	flag.Usage = usage
-	genModels.GetDriver(modelPath, driver, dsn, style, packageName).Run()
+	genModels.GormFormat.JsonUseCamel = false
+	if camel == "0" {
+		genModels.GormFormat.JsonUseCamel = true
+	}
+	fmt.Println(genModels.GormFormat,camel, style)
+	driver := genModels.GetDriver(modelPath, driver, dsn, style, packageName)
+	if ignoreTable != "" {
+		ignoreTables := strings.Split(ignoreTable, ",")
+		driver.SetIgnoreTables(ignoreTables...)
+	}
+	driver.Run()
 }
 
 func usage() {
